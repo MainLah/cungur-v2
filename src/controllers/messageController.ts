f50e7a407207dd84714480e0caf6069bc4b01c4d
Rendra Kusuma Blikololo <rendrakusumab@gmail.com>
2025-03-16 13:08:30 +0700

    feat: get and create messages

--- src/controllers/messageController.ts
+++ src/controllers/messageController.ts
@@ -1,11 +1,38 @@
 import { Request, Response } from 'express';
+import messageModel from '../models/messageModel';
 
 export default {
     async getMessages(req: Request, res: Response) {
-        res.send('GET request');
+        try {
+            const messages = await messageModel.find();
+            res.status(200).json({
+                message: 'Messages fetched successfully',
+                data: messages
+            })
+        }
+        catch (error) {
+            res.status(403).json({
+                message: 'Error fetching messages',
+                data: null
+            })
+        }
     },
     async createMessage(req: Request, res: Response) {
-        res.send('POST request');
+        const message = req.body.message;
+        const timestamp = new Date().toDateString();
+        try {
+            const newMessage = await messageModel.create({ message, timestamp });
+            res.status(200).json({
+                message: 'Message created successfully',
+                data: newMessage
+            })
+        }
+        catch (error) {
+            res.status(403).json({
+                message: 'Error creating message',
+                data: null
+            })
+        }
     }
 }
 
