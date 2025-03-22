75726eecde952cad2c245009d80fcd9b0ff88baf
MainLah <rendrakusumab@gmail.com>
2025-03-22 10:32:46 +0700

    chore: code consistency

--- src/controllers/messageController.ts
+++ src/controllers/messageController.ts
@@ -1,38 +1,35 @@
-import { Request, Response } from 'express';
-import messageModel from '../models/messageModel';
+import { Request, Response } from "express";
+import messageModel from "../models/messageModel";
 
 export default {
-    async getMessages(req: Request, res: Response) {
-        try {
-            const messages = await messageModel.find();
-            res.status(200).json({
-                message: 'Messages fetched successfully',
-                data: messages
-            })
-        }
-        catch (error) {
-            res.status(403).json({
-                message: 'Error fetching messages',
-                data: null
-            })
-        }
-    },
-    async createMessage(req: Request, res: Response) {
-        const message = req.body.message;
-        const timestamp = new Date().toDateString();
-        try {
-            const newMessage = await messageModel.create({ message, timestamp });
-            res.status(200).json({
-                message: 'Message created successfully',
-                data: newMessage
-            })
-        }
-        catch (error) {
-            res.status(403).json({
-                message: 'Error creating message',
-                data: null
-            })
-        }
+  async getMessages(req: Request, res: Response) {
+    try {
+      const messages = await messageModel.find();
+      res.status(200).json({
+        message: "Messages fetched successfully",
+        data: messages,
+      });
+    } catch (error) {
+      res.status(403).json({
+        message: "Error fetching messages",
+        data: null,
+      });
     }
-}
-
+  },
+  async createMessage(req: Request, res: Response) {
+    const message = req.body.message;
+    const timestamp = new Date().toDateString();
+    try {
+      const newMessage = await messageModel.create({ message, timestamp });
+      res.status(200).json({
+        message: "Message created successfully",
+        data: newMessage,
+      });
+    } catch (error) {
+      res.status(403).json({
+        message: "Error creating message",
+        data: null,
+      });
+    }
+  },
+};
--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,14 +1,13 @@
-import mongoose from 'mongoose';
-import { DB_URL } from './env';
+import mongoose from "mongoose";
+import { DB_URL } from "./env";
 
 export const connectDb = async () => {
-    try {
-        await mongoose.connect(DB_URL, {
-            dbName: 'cungur-db'
-        })
-        return Promise.resolve('Successfully connected to the database');
-    }
-    catch(error) {
-        return Promise.reject(error);
-    }
-}
+  try {
+    await mongoose.connect(DB_URL, {
+      dbName: "cungur-db",
+    });
+    return Promise.resolve("Successfully connected to the database");
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
